6b82aabba9e85e4b053f8112d981b166861ab671
Matias Martirian <matiasezequielmartirian@protonmail.com>
2025-01-18 16:16:31 -0400

    pito

--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -20,6 +20,7 @@ namespace GUI
         private void button1_Click(object sender, EventArgs e)
         {
             dataGridView1.Visible = false;
+            MessageBox.Show("pito");
             MessageBox.Show(":v");
         }
     }
